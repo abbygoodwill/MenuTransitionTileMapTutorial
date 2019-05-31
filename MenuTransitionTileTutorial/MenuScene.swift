ae9091ac8f336c65152e5040881679e74d344ba7
Abby Goodwill <abbygoodwill@Jamess-iMac-1.hsd1.co.comcast.net>
2019-05-31 14:46:46 -0600

    adding inits to MenuScene

--- MenuTransitionTileTutorial/MenuScene.swift
+++ MenuTransitionTileTutorial/MenuScene.swift
@@ -11,4 +11,16 @@ import SpriteKit
 
 class MenuScene: SKScene {
     
+    // initializing this class
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        
+    }
+    override init(size: CGSize) {
+        
+        super.init(size: size)
+        
+    
+    }
 }
