76356e52bf0e0a145932ada9b50ce533b5e3bf9f
Abby Goodwill <abbygoodwill@Jamess-iMac-1.hsd1.co.comcast.net>
2019-05-31 14:39:50 -0600

    adding empty MenuScene

---
+++ MenuTransitionTileTutorial/MenuScene.swift
@@ -0,0 +1,14 @@
+//
+//  MenuScene.swift
+//  MenuTransitionTileTutorial
+//
+//  Created by Abby Goodwill on 5/31/19.
+//  Copyright © 2019 Abby Goodwill. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MenuScene: SKScene {
+    
+}
